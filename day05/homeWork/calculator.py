82169ae7db3fb60e5d606481854a030b43ec3f4d
caiqinxiong <13269469526@163.com>
2019-08-06 14:46:05 +0800

    update home work

--- day05/homeWork/calculator.py
+++ day05/homeWork/calculator.py
@@ -2,54 +2,41 @@
 __author__ = 'caiqinxiong_cai'
 # 2019/8/2 17:00
 import re
-import functools
 
 
-def minus_operator_handler(num):
-    '''处理一些特殊的减号运算'''
-    minus_operators = re.split("-", num)
-    calc_list = re.findall("[0-9]", num)
-    if minus_operators[0] == '':  # 第一值肯定是负号
-        calc_list[0] = '-%s' % calc_list[0]
-    res = functools.reduce(lambda x, y: float(x) - float(y), calc_list)
-    print("\033[33;1m减号[%s]处理结果:\033[0m" % num, res)
-    return res
+def multiply_divide_compute(exp):
+    '''乘除计算'''
+    num_list = re.split("[*/]", exp)  # 根据*或/号切割式子，得到各个数字的list,如['7/3*99/4*2998']切割后为['7', '3', '99', '4', '2998']
+    symbol = re.findall("[*/]", exp)  # 再获取一一对应的计算符号，['/', '*', '/', '*']
+    ret = float(num_list[0])  # 从左到右依次计算，list第一个值赋给ret，然后去*或/下一个数，返回计算结果。
+    for n in range(1, len(num_list)):
+        if symbol[n - 1] == "*":
+            ret *= float(num_list[n])
+        elif symbol[n - 1] == "/":
+            ret /= float(num_list[n])
+    return ret
 
 
-def change_symbol(num):
-    num = num.replace("++", "+")
-    num = num.replace("+-", "-")
-    num = num.replace("-+", "-")
-    num = num.replace("--", "+")
-    num = num.replace("- -", "+")
-    return num
+def add_subtract_compute(symbol, num_list):
+    '''计算加减'''
+    ret = float(num_list[0])  # 从左到右依次计算，list第一个值赋给ret，然后去+或-下一个数，返回计算结果。
+    for n in range(1, len(num_list)):
+        if symbol[n - 1] == "+":
+            ret += float(num_list[n])
+        elif symbol[n - 1] == "-":
+            ret -= float(num_list[n])
+    return ret
 
 
-def compute_mutiply_and_dividend(num):
-    '''算乘除,传进来的是字符串噢'''
-    operators = re.findall("[*/]", num)
-    calc_list = re.split("[*/]", num)
-    res = None
-    for index, i in enumerate(calc_list):
-        if res:
-            if operators[index - 1] == "*":
-                res *= float(i)
-            elif operators[index - 1] == "/":
-                res /= float(i)
-        else:
-            res = float(i)
-
-    print("\033[31;1m[%s]运算结果=\033[0m" % num, res)
-    return res
-
-
-def handle_minus_in_list(operator_list, calc_list):
-    '''有的时候把算术符和值分开后,会出现这种情况  ['-', '-', '-'] [' ', '14969037.996825399 ', ' ', '12.0/ 10.0 ']
-       这需要把第2个列表中的空格都变成负号并与其后面的值拼起来,恶心死了
-    '''
-    for index, i in enumerate(calc_list):
-        if i == '':  # 它其实是代表负号,改成负号
-            calc_list[index + 1] = i + calc_list[index + 1].strip()
+def change_symbol(exp):
+    '''处理符号'''
+    exp = exp.replace("++", "+")
+    exp = exp.replace("+-", "-")
+    exp = exp.replace("-+", "-")
+    exp = exp.replace("--", "+")
+    exp = exp.replace("- -", "+")
+    exp = exp.replace("+ -", "-")
+    return exp
 
 
 def reduction_formula(symbol, multiply_divide):
@@ -57,70 +44,56 @@ def reduction_formula(symbol, multiply_divide):
     # 处理乘除算式中前面带负号的情况
     if len(multiply_divide[0].strip()) == 0:  # 切割后的list第一个值如果为空，则为负号，如-40/5切割后为['', '40/5']
         multiply_divide[1] = symbol[0] + multiply_divide[1]
-        del multiply_divide[0] # 把list的第一个空值删除掉
-        del symbol[0] # 负号已经拼接到上面的式子里了，所以要删除掉多余的负号。
+        del multiply_divide[0]  # 把list的第一个空值删除掉
+        del symbol[0]  # 负号已经拼接到上面的式子里了，所以要删除掉多余的负号。
     # 处理乘除算式中后面带负号的情况
-    for index, i in enumerate(multiply_divide): #9-2*5/3+7/3*99/4*-2998 +10 * 568/14 切割后为['9', '2*5/3 ', ' 7 /3*99/4*', '2998 ', '10 * 568/14 ']
-        i = i.strip()
-        if i.endswith("*") or i.endswith("/"):
-            multiply_divide[index] = multiply_divide[index] + symbol[index] + multiply_divide[index + 1]
-            del multiply_divide[index + 1]
-            del symbol[index]
+    for index, f in enumerate(multiply_divide):
+        # 9-2*5/3+7/3*99/4*-2998 +10 * 568/14 切割后为['9', '2*5/3 ', ' 7 /3*99/4*', '2998 ', '10 * 568/14 ']，循环list处理。
+        f = f.strip()  # 去除算式中的左右空格
+        if f.endswith("*") or f.endswith("/"):  # 如果以*或/结尾的式子，说明后面带-号被切割了，给还原一下。
+            multiply_divide[index] = multiply_divide[index] + symbol[index] + multiply_divide[
+                index + 1]  # 如：['', '40/', '5']还原为 ['-40/-5']
+            del multiply_divide[index + 1]  # 将已拼接的值删除掉
+            del symbol[index]  # 对应的负号也删除掉
     return symbol, multiply_divide
 
 
-def compute(num):
+def compute(exp):
     '''计算'''
     # 先计算乘除，后算加减。
-    multiply_divide = re.split("[+-]", num)  # 按+或-切割，取出乘除算式，返回list。-40/5切割后为['', '40/5']，-40/-5切割后为['', '40/', '5']
-    symbol = re.findall("[+-]", num) # 获取所有的+和—号，返回list，和上面的split的个数对应。-40/-5得到的值为['-', '-']
-    print("*"*200)
-    print(multiply_divide)
-    print(symbol)
-    symbol, multiply_divide = reduction_formula(symbol,multiply_divide)
-    print(symbol, multiply_divide)
-    exit(-1)
-    for index, i in enumerate(multiply_divide):
-        if re.search("[*/]", i):
-            sub_res = compute_mutiply_and_dividend(i)
-            multiply_divide[index] = sub_res
+    multiply_divide = re.split("[+-]", exp)  # 按+或-切割，取出乘除算式，返回list。-40/5切割后为['', '40/5']，-40/-5切割后为['', '40/', '5']
+    symbol = re.findall("[+-]", exp)  # 获取所有的+和—号，返回list，和上面的split的个数对应。-40/-5得到的值为['-', '-']
+    symbol, multiply_divide = reduction_formula(symbol, multiply_divide)  # 调用符号处理函数，返回处理后的结果。
+    # 开始计算乘除
+    for index, f in enumerate(multiply_divide):  # 可能有多个，循环计算，如['9', '2*5/3 ', ' 7 /3*99/4*-2998 ', '10 * 568/14 ']
+        if re.search("[*/]", f):  # 只计算乘除的式子，没有返回None。
+            multiply_divide[index] = multiply_divide_compute(f)  # 调用乘除计算函数，每次传入的是只带*或/的式子，将计算结果返回对应的list中，如['-40/5']计算完返回['-8.0']
 
-    # 开始运算+,-
-    print(multiply_divide, symbol)
-    total_res = None
-    for index, item in enumerate(multiply_divide):
-        if total_res:  #代表不是第一次循环
-            if symbol[index - 1] == '+':
-                total_res += float(item)
-            elif symbol[index - 1] == '-':
-                total_res -= float(item)
-        else:
-            total_res = float(item)
-    print("\033[32;1m[%s]运算结果:\033[0m" % num, total_res)
-    return total_res
+    # 开始计算加减
+    # print(symbol,multiply_divide) #如['-', '+', '+'] ['9', 3.3333333333333335, -173134.50000000003, 405.7142857142857]
+    ret = add_subtract_compute(symbol, multiply_divide)  # 将对应的+-符号和计算完乘除后的list再进行加减运算，返回最终的计算结果。
+    return ret
 
 
-def main(num):
+def main(exp):
     '''主控制逻辑'''
     while True:
-        parentheses = re.search("\([^()]*\)", num)  # [^()]*表示不包含任意一个口号，既匹配最里层的括号，从右往左，匹配第一个。
+        parentheses = re.search("\([^()]*\)", exp)  # [^()]*表示不包含任意一个口号，既匹配最里层的括号，从右往左，匹配第一个。
         # print(parentheses) # 通过调用group()方法得到匹配的字符串,如果字符串没有匹配，则返回None。
         if parentheses != None:
-            num = parentheses.group().strip("()")  # 去掉拓号，只保留算式。
-            print(num)
-            ret = compute(num) # 计算括号里的式子并返回计算结果
-            ret = change_symbol(ret)  # 计算完成后可能产生负号，去除外重复的+-号
-            num = num.replace(num, str(ret)) # 计算完成后，把括号里的内容替换成计算结果
+            f = parentheses.group().strip("()")  # 去掉拓号，只保留算式。
+            ret = compute(f)  # 计算括号里的式子并返回计算结果
+            exp = exp.replace(parentheses.group(), str(ret))  # 计算完成后，把括号里的内容替换成计算结果
+            exp = change_symbol(exp)  # 一个括号计算完成后可能产生负号，处理+-号，例如计算完第一个括号的结果为-8.0，1 - 2 * ( (60-30 +-8.0 * (9-2*5/3 + 7 /3*99/4*2998 +10 * 568/14 )) - (-4*3)/ (16-3*2) )，带了+-号。
         else:
-            # 没有括号了，直接计算剩下的数即可
-            ret = compute(num)
-            print('\n\n\033[42;1m最终结果:\033[0m', ret)
-            break
+            ret = compute(exp) # 没有括号了，直接计算剩下的数即可
+            return ret
 
 
 if __name__ == '__main__':
-    exp = '(9-2*5/3 + 7 /3*99/4*-2998 +10 * 568/14 )'
-    #exp = '(-40/-5)'
-    #exp = '1 - 2 * ( (60-30 +(-40/5) * (9-2*5/3 + 7 /3*99/4*2998 +10 * 568/14 )) - (-4*3)/ (16-3*2) )'
-    #exp ="1 - 2 * ( (60-30 +(-9-2-5-2*3-5/3-40*4/2-3/5+6*3) * (-9-2-5-2*5/3 + 7 /3*99/4*2998 +10 * 568/14 )) - (-4*3)/ (16-3*2) )"
-    res = main(exp)
+    # exp = '(-40/5)'
+    # exp = '( 7 /3*99/4*2998 )'
+    # exp = '(9-2*5/3 + 7 /3*99/4*-2998 +10 * 568/14 )'
+    exp = '1 - 2 * ( (60-30 +(-40/5) * (9-2*5/3 + 7 /3*99/4*2998 +10 * 568/14 )) - (-4*3)/ (16-3*2) )'
+    ret = main(exp)
+    print('算式%s的计算结果为：\n%s' % (exp, ret))
